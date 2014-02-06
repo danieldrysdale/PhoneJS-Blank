d3ce06ac935214a042347aea70f38cb3b0d967b3
Daniel Drysdale <daniel@drysdale.org>
2014-02-06 22:02:48 +1100

    Removed hardcoded ios look & feel

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
     // Uncomment the line below to disable platform-specific look and feel and to use the Generic theme for all devices
     // DevExpress.devices.current({ platform: "generic" });
 
-    DevExpress.devices.current({ platform: "ios" });
+//    DevExpress.devices.current({ platform: "ios" });
 
     $(function() {
         MyApp.app = new DevExpress.framework.html.HtmlApplication({
